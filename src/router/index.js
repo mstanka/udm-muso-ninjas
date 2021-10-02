0c79dec05ff4f3bda2e8b7e79766979242fcd162
mstanka <stmmarketa@gmail.com>
2021-10-02 10:07:42 +0200

    :rocket: add CreatePlaylist component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'
-import Login from '../views/auth/Login.vue'
+import Home from '../views/Home.vue';
+import Login from '../views/auth/Login.vue';
 import Signup from '../views/auth/Signup.vue';
-
+import CreatePlaylist from '../views/playlists/CreatePlaylist.vue';
 
 const routes = [
   {
@@ -19,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup,
+  },
+  {
+    path: '/playlists/create',
+    name: 'CreatePlaylist',
+    component: CreatePlaylist,
   },
 ];
 

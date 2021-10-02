c3934ab9211f347f49659f87d9262afbb6642ab3
mstanka <stmmarketa@gmail.com>
2021-10-02 20:50:00 +0200

    add route guards

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,7 @@
 
       <div class="links">
         <div v-if="user">
+          <router-link :to="{ name: 'CreatePlaylist' }">Create Playlist</router-link>
           <button class="btn" @click="handleClick">Logout</button>
         </div>
         <div v-else>

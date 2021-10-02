61e0e5dd47503966e8d7f9ddcf5da2fa0ec8c2ed
mstanka <stmmarketa@gmail.com>
2021-10-02 10:33:39 +0200

    :rocket: upload image to firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
-import 'firebase/storage'
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBjaYQlEgQcHOC8BlRPNX2S2qOqwvMJArI',
@@ -16,7 +16,7 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
-const projectStorage = firebase.storage()
+const projectStorage = firebase.storage();
 
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 

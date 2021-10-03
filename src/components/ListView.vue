919d251c3e5ddc86f3f7eb2e917c26011d9f4e9f
mstanka <stmmarketa@gmail.com>
2021-10-03 11:06:51 +0200

    :rocket: show playlist details

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,17 +1,19 @@
 <template>
   <div v-for="playlist in playlists" :key="playlist.id">
-    <div class="single">
-      <div class="thumbnail">
-        <img :src="playlist.coverUrl" :alt="playlist.title" />
+    <router-link :to="{ name: 'PlaylistDetails', params: { id: playlist.id } }">
+      <div class="single">
+        <div class="thumbnail">
+          <img :src="playlist.coverUrl" :alt="playlist.title" />
+        </div>
+        <div class="info">
+          <h3>{{ playlist.title }}</h3>
+          <p>Created by {{ playlist.userName }}</p>
+        </div>
+        <div class="song-number">
+          <p>{{ playlist.songs.length }} songs</p>
+        </div>
       </div>
-      <div class="info">
-        <h3>{{ playlist.title }}</h3>
-        <p>Created by {{ playlist.userName }}</p>
-      </div>
-      <div class="song-number">
-        <p>{{ playlist.songs.length }} songs</p>
-      </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
---
+++ src/views/playlists/PlaylistDetails.vue
@@ -0,0 +1,73 @@
+<template>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" :alt="playlist.title" />
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by: {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+    <!-- song list -->
+    <div class="song-list">
+      <p>Song list here</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import getDocument from '../../composables/getDocument';
+
+export default {
+  props: ['id'],
+  setup(props) {
+    const { error, document: playlist } = getDocument('playlists', props.id);
+
+    return { error, playlist };
+  },
+};
+</script>
+
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 80px;
+}
+
+@media screen and (min-width: 660px) {
+  .playlist-details {
+    grid-template-columns: 1fr 2fr;
+  }
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+}
+.cover img {
+  display: block;
+  max-width: 600px;
+  width: 100%;
+  object-fit: cover;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
+</style>
